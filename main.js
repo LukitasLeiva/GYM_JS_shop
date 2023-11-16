60b2a03b1b5ee7445adeebb032f3804986a9ac9f
Lucas Leiva <leivalucas0903@gmail.com>
2023-11-16 01:50:30 -0300

    suplementos

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-const vueltas = prompt("ingrese vueltas a dar")
-let signo = "positivo"
-let numero = 0
+let opcion = 0
+let preciosuplemento = 0
+opcion = prompt("ingrese plan a seleccionar: \n1. plan alimenticio\n2. rutina de entrenamiento\n3. suplementos")
 
-for (let i=1; i<=vueltas; i+=1){
-    console.log ("vuelta: ", i)
-    let numero = parseInt(prompt("ingrese un numero"))
-    if (numero>=0){
-        signo="positivo"
-    } else{
-        signo="negativo"
+if (opcion==1) {
+    let planalimenticio = prompt("ingrese su plan ideal\n")
+}
+if (opcion==2) {
+    // tambien vamos a hacer un tipo ticket
+}
+if (opcion==3) {
+    let tipodesuplemento = prompt("ingrese que suplemento va a necesitar(creatina, proteína o preentreno):")
+    tipodesuplemento = tipodesuplemento.toLowerCase()
+    switch(tipodesuplemento){
+        case "creatina":
+            preciosuplemento = "el valor de la creatina $15000"
+            alert(preciosuplemento)
+            break;
+        case "proteina","proteína":
+            preciosuplemento = "el valor de la proteina $10000"
+            alert(preciosuplemento)
+            break;
+        case "preentreno":
+            preciosuplemento = "el valor del preentreno $15000"
+            alert(preciosuplemento)
+            break;
+        default:
+            alert("no hay stock")
     }
-    console.log ("su numero es", numero, "y es", signo)
 }
