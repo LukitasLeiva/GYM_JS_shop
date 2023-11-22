8c2aa53f0034d9ce2e30a2a15b9a15a066c9a783
Lucas Leiva <leivalucas0903@gmail.com>
2023-11-22 20:24:39 -0300

    cambios js

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
 let opcion = 0
 let preciosuplemento = 0
-opcion = prompt("ingrese plan a seleccionar: \n1. plan alimenticio\n2. rutina de entrenamiento\n3. suplementos")
 
-if (opcion==1) {
-    let planalimenticio = prompt("ingrese su plan ideal\n")
-}
-if (opcion==2) {
-    // tambien vamos a hacer un tipo ticket
-}
-if (opcion==3) {
-    let tipodesuplemento = prompt("ingrese que suplemento va a necesitar(creatina, proteína o preentreno):")
-    tipodesuplemento = tipodesuplemento.toLowerCase()
-    switch(tipodesuplemento){
-        case "creatina":
-            preciosuplemento = "el valor de la creatina $15000"
-            alert(preciosuplemento)
-            break;
-        case "proteina","proteína":
-            preciosuplemento = "el valor de la proteina $10000"
-            alert(preciosuplemento)
-            break;
-        case "preentreno":
-            preciosuplemento = "el valor del preentreno $15000"
-            alert(preciosuplemento)
-            break;
-        default:
-            alert("no hay stock")
+
+function consulta(){
+    opcion = prompt("ingrese plan a seleccionar: \n1. plan alimenticio\n2. rutina de entrenamiento\n3. suplementos")
+    if (opcion==1) {
+        
+    }
+    if (opcion==2) {
+        // tambien vamos a hacer un tipo ticket
+    }
+    if (opcion==3) {
+        let tipodesuplemento = prompt("ingrese que suplemento va a necesitar(creatina, proteína o preentreno):")
+        tipodesuplemento = tipodesuplemento.toLowerCase()
+        switch(tipodesuplemento){
+            case "creatina":
+                preciosuplemento = "el valor de la creatina $15000"
+                alert(preciosuplemento)
+                break;
+            case "proteina","proteína":
+                preciosuplemento = "el valor de la proteina $10000"
+                alert(preciosuplemento)
+                break;
+            case "preentreno":
+                preciosuplemento = "el valor del preentreno $15000"
+                alert(preciosuplemento)
+                break;
+            default:
+                alert("no hay stock")
+        }
     }
 }
+
+
+
